74ae858b8b9dabb9f88dc31eabbe243bd9df586e
Timothy Chelelgo <timmybarkuch007@gmail.com>
2025-04-22 16:20:00 +0300

    - Created an animated Counter - Added more constants to the index.js -Finished the Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,27 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import React from "react";
+import AnimatedCounter from "../components/AnimatedCounter";
 import Button from "../components/Button";
 import HeroExperience from "../components/HeroModels/HeroExperience";
 import { words } from "../constants";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo('.hero-text h1',
+      {
+        y: 50,
+        opcaity: 0
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.2,
+        duration: 1,
+        ease: 'power2.inOut'
+      }
+    )
+  })
   return (
     <section id="hero" className="relative w-full overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
@@ -53,11 +71,12 @@ const Hero = () => {
 
         {/* RIGHT: 3D MODEL*/}
         <figure>
-            <div className="hero-3d-layout border-red-200 border-2">
+            <div className="hero-3d-layout">
                 <HeroExperience/>
             </div>
         </figure>
       </div>
+      <AnimatedCounter />
     </section>
   );
 };
