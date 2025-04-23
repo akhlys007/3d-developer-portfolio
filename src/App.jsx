287e950f4bfc5b4c2e81ca92f97bc8150a353471
Timothy Chelelgo <timmybarkuch007@gmail.com>
2025-04-23 05:56:11 +0300

    -Created LogoShowcase, FeatureCards and Navbar sections -Added section imports to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import NavBar from "./components/NavBar"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
+import LogoShowcase from "./sections/LogoShowcase"
 import ShowcaseSection from "./sections/ShowcaseSection"
+
 
 const App = () => {
   return (
     <>
+    <NavBar/>
     <Hero/>
     <ShowcaseSection/>
+    <LogoShowcase/>
+    <FeatureCards/>
     </>
   )
 }
